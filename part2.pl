8f9c6ad0fd4c47e665ccda806800ec6e923dafea
Leo Cao <z5261846@ad.unsw.edu.au>
2021-04-22 01:45:25 +1000

    part 2 up to 2.3

---
+++ part2.pl
@@ -0,0 +1,38 @@
+:- op(300, xfx, <-).
+inter_construction(X <- B1, Y <- B2, X <- NewX, Y <- NewY, Z <- B) :-
+    X \= Y,
+    intersection(B1, B2, B),
+    gensym(z, Z),
+    subtract(B1, B, RestOfB1),
+    subtract(B2, B, RestOfB2),
+    append(RestOfB1, [Z], NewX),
+    append(RestOfB2, [Z], NewY).
+
+intra_construction(X <- B1, Y <- B2, X <- C1, Z <- C2, Z <- C3) :-
+    X == Y,
+    intersection(B1, B2, I),
+    gensym(z, Z),
+    append(I, [Z], C1),
+    subtract(B1, I, C2),
+    subtract(B2, I, C3).
+
+absorption(X <- B1, Y <- B2, X <- C1, Y <- C2) :-
+    subset(B2,B1),
+    C2 = B2,
+    subtract(B1, B2, RestOfB1),
+    append([Y], RestOfB1, C1).
+
+identification(X <- B1, Y <- B2, X <- C1, Y <- C2) :-
+    X == Y,
+    intersection(B1, B2, I),
+    subtract(B1, I, RestOfB1),
+    subtract(B2, I, RestOfB2),
+    (length(RestOfB1, 1) -> (C1 = B1, C2 = RestOfB2);
+    length(RestOfB2, 1) -> (C1 = B2, C2 = RestOfB1)).
+
+
+
+% test(List1, List2):-
+%     length(List1, 1);
+%     length(List2, 1),
+
